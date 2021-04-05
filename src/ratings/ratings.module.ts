158116dce54400e5adb80a29217cae8654d95958
Rishi Kothari <itsrishikothari@gmail.com>
2021-04-05 09:02:56 -0400

    rating module

--- src/ratings/ratings.module.ts
+++ src/ratings/ratings.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common'
 import { RatingService } from './service/rating.service'
 import { RatingResolver } from './resolver/rating.resolver'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Rating } from './rating'
 
 @Module({
 	providers: [RatingService, RatingResolver],
+	imports: [
+		TypeOrmModule.forFeature([
+			Rating
+		])
+	]
 })
 export class RatingsModule {}
