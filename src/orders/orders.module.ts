6e502fb99b65d222a816a7dd35d55f1d24590f9a
Rishi Kothari <itsrishikothari@gmail.com>
2021-04-05 09:03:02 -0400

    order module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common'
 import { OrderService } from './service/order.service'
-import { OrderResolver } from './resolver/order.resolver'
+import { OrderFieldResolver, OrderResolver } from './resolver/order.resolver'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from '../users/user'
+import { Order } from './order'
 
 @Module({
-	providers: [OrderService, OrderResolver],
+	providers: [OrderService, OrderResolver, OrderFieldResolver],
+	imports: [
+		TypeOrmModule.forFeature([
+			User,
+			Order,
+		])
+	]
 })
 export class OrdersModule {}
