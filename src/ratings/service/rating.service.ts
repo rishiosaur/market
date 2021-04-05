a366b2283526114fbe16701f56341456b0be80be
Rishi Kothari <itsrishikothari@gmail.com>
2021-04-05 09:02:51 -0400

    rating service

--- src/ratings/service/rating.service.ts
+++ src/ratings/service/rating.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common'
+import { BaseEntity, Repository } from 'typeorm'
+import { BaseEntityService } from '../../shared/database'
+import { Rating } from '../rating'
+import { InjectRepository } from '@nestjs/typeorm'
 
 @Injectable()
-export class RatingService {}
+export class RatingService extends BaseEntityService(Rating) {
+	constructor(@InjectRepository(Rating) private ratings: Repository<Rating>) {
+		super(ratings);
+	}
+}
