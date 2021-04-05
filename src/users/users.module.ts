24fcd263b62db90bf8d83e7ad01cebe9801fd1b1
Rishi Kothari <itsrishikothari@gmail.com>
2021-04-05 09:02:20 -0400

    user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common'
 import { UserService } from './service/user.service'
-import { UserResolver } from './resolver/user.resolver'
+import { UserFieldResolver, UserResolver } from './resolver/user.resolver'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './user'
+import { Rating } from '../ratings/rating'
+import { Order } from '../orders/order'
 
 @Module({
-	providers: [UserService, UserResolver],
+	providers: [UserService, UserResolver, UserFieldResolver],
+	imports: [
+		TypeOrmModule.forFeature([
+			User,
+			Rating,
+			Order
+		])
+	]
 })
 export class UsersModule {}
