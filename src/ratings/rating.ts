0ec49c9403700e7140e8e7969d9972770bd900e0
Rishi Kothari <itsrishikothari@gmail.com>
2021-04-05 09:02:13 -0400

    rating entity

--- src/ratings/rating.ts
+++ src/ratings/rating.ts
@@ -1,1 +1,44 @@
-export class Rating {}
+import { User } from '../users/user'
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { Field, ID, ObjectType, registerEnumType } from '@nestjs/graphql'
+
+export enum RatingType {
+	Selling,
+	Buying
+}
+
+registerEnumType(RatingType, {
+	name: "RatingType"
+})
+
+@Entity()
+@ObjectType()
+export class Rating {
+	@Field(() => ID)
+	@PrimaryGeneratedColumn("uuid")
+	id: string
+
+	@Field(() => User)
+	@ManyToOne(() => User, u => u.writtenRatings)
+	from: User
+
+	@Field(() => User)
+	@ManyToOne(() => User, u=> u.ratings)
+	for: User
+
+	@Field()
+	@Column()
+	description: string
+
+	@Field()
+	@CreateDateColumn()
+	created: Date
+
+	@Field()
+	@Column()
+	rating: number
+
+	@Field(() => RatingType)
+	@Column()
+	type: RatingType
+}
