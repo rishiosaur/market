5cbdab38d531c8175a3a60dae4b5bb5403cf0595
Rishi Kothari <itsrishikothari@gmail.com>
2021-04-05 09:02:17 -0400

    user entity

--- src/users/user.ts
+++ src/users/user.ts
@@ -1,17 +1,19 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql'
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
+import { Column, Entity, ManyToMany, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm'
 import { Order } from '../orders/order'
+import { Rating } from '../ratings/rating'
 
 @ObjectType()
 @Entity()
 export class User {
-	@PrimaryGeneratedColumn('uuid')
+	@PrimaryColumn()
 	@Field(() => ID)
 	id: string
 
+	@Field(() => [Order])
 	@OneToMany(() => Order, (o) => o.seller)
 	selling: Order[]
-
+@Field(() => [Order])
 	@OneToMany(() => Order, (o) => o.buyer)
 	buying: Order[]
 
@@ -22,4 +24,16 @@ export class User {
 	@Field()
 	@Column()
 	buyingRating: number
+
+	@Field(() => [Rating])
+	@OneToMany(() => Rating, r => r.from)
+	writtenRatings: Rating[]
+
+	@Field(() => [Rating])
+	@OneToMany(() => Rating, r => r.for)
+	ratings: Rating[]
+
+	@Field(() => [Order])
+	@ManyToMany(() => Order, o => o.requests)
+	requests: Order[]
 }
