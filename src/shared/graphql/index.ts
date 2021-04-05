06db7000014b9688f7f76af881f02b3dad4c631c
Rishi Kothari <itsrishikothari@gmail.com>
2021-04-05 09:02:45 -0400

    graphql base resolver

---
+++ src/shared/graphql/index.ts
@@ -0,0 +1,43 @@
+import { Type } from '@nestjs/common'
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql'
+import { BaseEntityService, IBaseEntityService } from '../database'
+import { plural } from 'pluralize'
+import { mapPaginationToFindProps, PaginationInput } from './Pagination'
+
+export const BaseEntityResolver = <Entity extends Type<unknown>>(
+	associatedEntityReference: Entity
+): any => {
+	@Resolver({ isAbstract: true })
+	abstract class AbstractBase {
+		constructor(private service: IBaseEntityService<Entity, any>) {}
+
+		@Query(() => [associatedEntityReference], {
+			name: plural(associatedEntityReference.name).toLowerCase(),
+		})
+		async findMany(
+			@Args('options', { nullable: true }) options: PaginationInput
+		): Promise<Entity[]> {
+			return this.service.findMany({
+				...mapPaginationToFindProps(options),
+			})
+		}
+
+		@Query(() => associatedEntityReference, {
+			name: associatedEntityReference.name.toLowerCase(),
+		})
+		async find(@Args('id') id: string): Promise<Entity> {
+			return this.service.find(id)
+		}
+
+		@Query(() => [associatedEntityReference], {
+			name: `bulk${plural(associatedEntityReference.name)}`,
+		})
+		async findByIDs(
+			@Args('ids', { type: () => [String] }) ids: string[]
+		): Promise<Entity[]> {
+			return this.service.findByIDs(ids)
+		}
+	}
+
+	return AbstractBase
+}
