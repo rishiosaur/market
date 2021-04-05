0e4dfdac4a40f02a884053aea7f7686a0b7988d5
Rishi Kothari <itsrishikothari@gmail.com>
2021-04-05 09:02:54 -0400

    rating resolver

--- src/ratings/resolver/rating.resolver.ts
+++ src/ratings/resolver/rating.resolver.ts
@@ -1,4 +1,12 @@
 import { Resolver } from '@nestjs/graphql'
+import { BaseEntityResolver } from '../../shared/graphql'
+import { Rating } from '../rating'
+import { RatingService } from '../service/rating.service'
 
 @Resolver()
-export class RatingResolver {}
+export class RatingResolver extends BaseEntityResolver(Rating) {
+	constructor(private ratings: RatingService) {
+		super(ratings);
+
+	}
+}
